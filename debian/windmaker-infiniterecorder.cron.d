416bec86b8d6b2e05aaa2aebcd2b19c74f6f7bc9
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-09-13 20:48:06 +0000

    Change cron schedule

--- debian/windmaker-infiniterecorder.cron.d
+++ debian/windmaker-infiniterecorder.cron.d
@@ -1,1 +1,1 @@
-*/2 * * * * root flock -x -w 30 /tmp/windmaker-infiniterecorder-video-manager.lock -c '/usr/bin/windmaker-infiniterecorder-video-manager'
+0 2 * * * root flock -x -w 30 /tmp/windmaker-infiniterecorder-video-manager.lock -c '/usr/local/bin/windmaker-infiniterecorder-video-manager'
