b6ce8e003e3732c3c99124741e7dbf916604f371
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-09-16 16:41:04 +0000

    Contiue writing postinstall script

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+# Prepare an array with regular users with /bin/bash as shell
+
+readarray allowed_users <<<$(awk -F ':' '$3>=1000 && $3 < 65534 && $7 == "/bin/bash"  {print $1}' </etc/passwd)
+
+declare -a dialog_options
+
+declare -i array_index
+array_index=1
+for allowed_user in "${allowed_users[@]}"; do
+	#
+	dialog_options+=("${array_index}" "${allowed_user}" off)
+	array_index=$((array_index + 1))
+done
+
+dialog_properties=(
+	--title "User selection"
+	--stdout
+	--radiolist "Select user where infinite recorder services will be installed:" 0 0 2
+)
+
+selected_option=$(dialog "${dialog_properties[@]}" "${dialog_options[@]}")
+
+if [ -z "${selected_option}" ]; then
+	echo "No user was set, aborting."
+	exit 1
+else
+
+	echo "selected_option ${selected_option}"
+
+	selected_user="${allowed_users[${selected}]}"
+
+	echo "selected_user ${selected_user}"
+
+fi
