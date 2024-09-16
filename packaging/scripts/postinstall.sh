35d94f509c20c841c8d8e5488212997269e5ba92
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-09-16 21:18:13 +0000

    Force dialog in postinstall

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -6,39 +6,36 @@ declare selected_user
 
 if [ -z "${TARGET_USER}" ]; then
 
-	if tty; then
+	readarray allowed_users <<<$(awk -F ':' '$3>=1000 && $3 < 65534 && $7 == "/bin/bash"  {print $1}' </etc/passwd)
 
-		readarray allowed_users <<<$(awk -F ':' '$3>=1000 && $3 < 65534 && $7 == "/bin/bash"  {print $1}' </etc/passwd)
+	declare -a dialog_options
 
-		declare -a dialog_options
+	declare -i array_index
+	array_index=1
+	for allowed_user in "${allowed_users[@]}"; do
+		dialog_options+=("${array_index}" "${allowed_user}" off)
+		array_index=$((array_index + 1))
+	done
 
-		declare -i array_index
-		array_index=1
-		for allowed_user in "${allowed_users[@]}"; do
-			dialog_options+=("${array_index}" "${allowed_user}" off)
-			array_index=$((array_index + 1))
-		done
+	dialog_properties=(
+		--title "User selection"
+		--stdout
+		--radiolist "Select user where infinite recorder services will be installed:" 0 0 2
+	)
 
-		dialog_properties=(
-			--title "User selection"
-			--stdout
-			--radiolist "Select user where infinite recorder services will be installed:" 0 0 2
-		)
+	selected_option=$(dialog "${dialog_properties[@]}" "${dialog_options[@]}")
 
-		selected_option=$(dialog "${dialog_properties[@]}" "${dialog_options[@]}")
+	if [ -z "${selected_option}" ]; then
+		echo "No user was set, aborting."
+		exit 1
+	else
 
-		if [ -z "${selected_option}" ]; then
-			echo "No user was set, aborting."
-			exit 1
-		else
+		echo "selected_option ${selected_option}"
 
-			echo "selected_option ${selected_option}"
+		selected_user=$(echo "${allowed_users[${selected}]}" | xargs echo -n)
 
-			selected_user=$(echo "${allowed_users[${selected}]}" | xargs echo -n)
+		echo "selected_user #${selected_user}#"
 
-			echo "selected_user #${selected_user}#"
-
-		fi
 	fi
 else # Target user is defined
 	selected_user="${TARGET_USER}"
